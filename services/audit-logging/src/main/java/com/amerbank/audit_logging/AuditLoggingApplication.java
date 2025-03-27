19b1f48946a2133ac4757a61b8d928de012af821
lfav07 <lfavieira07@gmail.com>
2025-03-27 14:38:22 -0300

    First Commit

---
+++ services/audit-logging/src/main/java/com/amerbank/audit_logging/AuditLoggingApplication.java
@@ -0,0 +1,13 @@
+package com.amerbank.audit_logging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuditLoggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuditLoggingApplication.class, args);
+	}
+
+}
