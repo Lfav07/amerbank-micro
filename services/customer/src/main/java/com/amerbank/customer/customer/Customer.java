fa1db9944d329ad8d299314290adc578249f29a7
lfav07 <lfavieira07@gmail.com>
2025-03-28 16:07:51 -0300

    Customer entity added

---
+++ services/customer/src/main/java/com/amerbank/customer/customer/Customer.java
@@ -0,0 +1,46 @@
+package com.amerbank.customer.customer;
+
+import jakarta.persistence.*;
+import jakarta.validation.Valid;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "customers")
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Valid
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private LocalDate dateOfBirth;
+
+    private boolean kycVerified;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+}
