4823c2e253543da726ec7529a4163b3e46786c19
lfav07 <lfavieira07@gmail.com>
2025-04-02 17:54:44 -0300

    feat(customer): added customer request dto

---
+++ services/customer/src/main/java/com/amerbank/customer/customer/CustomerRequest.java
@@ -0,0 +1,28 @@
+package com.amerbank.customer.customer;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import java.time.LocalDate;
+
+public record CustomerRequest(
+        Long id,
+
+        @NotNull(message = "Customer first name is required")
+        String firstName,
+
+        @NotNull(message = "Customer last name is required")
+        String lastName,
+
+        @NotNull(message = "Password is required")
+        String password,
+
+        @NotNull(message = "Customer Email is required")
+        @Email(message = "Customer Email is not a valid email address")
+        String email,
+
+        @NotNull(message = "Date of birth is required")
+        LocalDate dateOfBirth,
+
+        boolean kycVerified
+) {
+}
