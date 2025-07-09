ecf4e83409c81bc3a5fa74b60f8c1071045a9f2f
lfav07 <lfavieira07@gmail.com>
2025-07-08 23:19:32 -0300

    fix: changed exception's type to RuntimeException

--- services/auth-server/src/main/java/com/amerbank/auth_server/UserNotFoundException.java
+++ services/auth-server/src/main/java/com/amerbank/auth_server/UserNotFoundException.java
@@ -1,6 +1,6 @@
 package com.amerbank.auth_server;
 
-public class UserNotFoundException extends Exception {
+public class UserNotFoundException extends RuntimeException {
     public UserNotFoundException(String s) {
     }
 }
