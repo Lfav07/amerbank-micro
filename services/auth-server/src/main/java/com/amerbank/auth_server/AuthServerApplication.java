7f376bd21806ccc15f2c704a71476aaa65b876b8
lfav07 <lfavieira07@gmail.com>
2025-07-09 23:13:28 -0300

    fix: enabled scanning for the User entity

--- services/auth-server/src/main/java/com/amerbank/auth_server/AuthServerApplication.java
+++ services/auth-server/src/main/java/com/amerbank/auth_server/AuthServerApplication.java
@@ -2,8 +2,10 @@ package com.amerbank.auth_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.amerbank.auth_server.model")
 public class AuthServerApplication {
 
 	public static void main(String[] args) {
