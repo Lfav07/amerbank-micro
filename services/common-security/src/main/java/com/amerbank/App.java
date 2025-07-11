13ac11b98f2ccec7162eaa9d452f5357e35fc834
lfav07 <lfavieira07@gmail.com>
2025-07-11 01:10:01 -0300

    common-security module created

---
+++ services/common-security/src/main/java/com/amerbank/App.java
@@ -0,0 +1,13 @@
+package com.amerbank;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
