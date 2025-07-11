514fd15610cc9853a5e344d68cb6c9270f568d22
lfav07 <lfavieira07@gmail.com>
2025-07-11 01:12:07 -0300

    created login and register methods

--- services/customer/src/main/java/com/amerbank/customer/customer/controller/CustomerController.java
+++ services/customer/src/main/java/com/amerbank/customer/customer/controller/CustomerController.java
@@ -1,4 +1,42 @@
 package com.amerbank.customer.customer.controller;
 
+import com.amerbank.common_dto.AuthenticationResponse;
+import com.amerbank.common_dto.UserLoginRequest;
+import com.amerbank.customer.customer.dto.CustomerRequest;
+import com.amerbank.customer.customer.model.Customer;
+import com.amerbank.customer.customer.service.CustomerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/customer")
 public class CustomerController {
+
+    private CustomerService service;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerCustomer(CustomerRequest request) {
+        service.registerCustomer(request);
+        return  ResponseEntity.ok("Customer successfully registered");
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody UserLoginRequest request) {
+        try {
+            AuthenticationResponse auth = service.login(request);
+            return ResponseEntity.ok(auth);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(401).body("Invalid credentials");
+        } catch (IllegalStateException e) {
+            return ResponseEntity.status(500).body("Auth service unavailable");
+        }
+    }
+
+
+
 }
