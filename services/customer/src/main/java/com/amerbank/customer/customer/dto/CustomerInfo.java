a60e5dbc752a0803100c88b7c6d4ba5b7c66537f
lfav07 <lfavieira07@gmail.com>
2025-07-15 18:07:43 -0300

    refactor(customer) CustomerInfo dto now includes id

--- services/customer/src/main/java/com/amerbank/customer/customer/dto/CustomerInfo.java
+++ services/customer/src/main/java/com/amerbank/customer/customer/dto/CustomerInfo.java
@@ -3,6 +3,7 @@ package com.amerbank.customer.customer.dto;
 import java.time.LocalDate;
 
 public record CustomerInfo(
+        Long id,
         String firstName,
         String lastName,
         LocalDate dateOfBirth
