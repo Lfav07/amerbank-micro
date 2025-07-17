49cdf26e55ddd9431d7649d8b19944d798874cb3
lfav07 <lfavieira07@gmail.com>
2025-07-17 18:09:08 -0300

    feat(account) initialized account to the db

---
+++ services/account/src/main/resources/db/migration/account/V1__create_accounts_table.sql
@@ -0,0 +1,11 @@
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
+CREATE TABLE accounts (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    account_number VARCHAR(20) NOT NULL UNIQUE,
+    customer_id BIGINT NOT NULL,
+    balance NUMERIC(19,2) NOT NULL,
+    type VARCHAR(20) NOT NULL,
+    status VARCHAR(20) NOT NULL,
+    created_at TIMESTAMP DEFAULT now()
+);
