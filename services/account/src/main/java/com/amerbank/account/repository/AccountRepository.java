c653cf3e899efed50a3e4e01b9939d67211de1c2
lfav07 <lfavieira07@gmail.com>
2025-07-16 21:42:26 -0300

    feat(account) new method to find all accounts by a user id

--- services/account/src/main/java/com/amerbank/account/repository/AccountRepository.java
+++ services/account/src/main/java/com/amerbank/account/repository/AccountRepository.java
@@ -3,6 +3,7 @@ package com.amerbank.account.repository;
 import com.amerbank.account.model.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -10,4 +11,6 @@ public interface AccountRepository extends JpaRepository<Account, UUID> {
     boolean existsByAccountNumber(String accountNumber);
     Optional<Account> findByAccountNumber(String accountNumber);
     Optional<Account> findByCustomerId(Long CustomerID);
+
+    List<Account> findAllByCustomerId(UUID customerId);
 }
