46f8dc141452b4092a5ac703da60fa02fc3414cd
lfav07 <lfavieira07@gmail.com>
2025-07-17 18:07:24 -0300

    refactor(account) removed status from dto

--- services/account/src/main/java/com/amerbank/account/dto/AccountRequest.java
+++ services/account/src/main/java/com/amerbank/account/dto/AccountRequest.java
@@ -1,17 +1,10 @@
 package com.amerbank.account.dto;
 
-import com.amerbank.account.model.AccountStatus;
 import com.amerbank.account.model.AccountType;
-import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotNull;
-
-import java.math.BigDecimal;
 
 public record AccountRequest(
 
         @NotNull
-        AccountType type,
-
-        @NotNull
-        AccountStatus status
+        AccountType type
 ) {}
