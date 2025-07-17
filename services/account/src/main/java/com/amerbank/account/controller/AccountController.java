d76361172f2a0130e3a58d120b59c019c86f2008
lfav07 <lfavieira07@gmail.com>
2025-07-17 18:06:00 -0300

    feat(account) added a registerAccount method

--- services/account/src/main/java/com/amerbank/account/controller/AccountController.java
+++ services/account/src/main/java/com/amerbank/account/controller/AccountController.java
@@ -1,6 +1,14 @@
 package com.amerbank.account.controller;
 
+import com.amerbank.account.dto.AccountRequest;
+import com.amerbank.account.service.AccountService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +16,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/account")
 @RequiredArgsConstructor
 public class AccountController {
+
+    private final AccountService accountService;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerAccount(@RequestBody AccountRequest accountRequest, Authentication authentication, HttpServletRequest request) {
+        if (authentication == null || !authentication.isAuthenticated()) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
+        String authHeader = request.getHeader("Authorization");
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+        String jwtToken = authHeader.substring(7);
+        accountService.createAccount(accountRequest, jwtToken);
+        return ResponseEntity.ok("Account successfully registered");
+    }
 }
