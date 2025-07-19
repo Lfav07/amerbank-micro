84ca38c7847a443246f8757ac4b245ec20b80b1c
lfav07 <lfavieira07@gmail.com>
2025-07-18 22:36:50 -0300

    feat(account) new db queries methods

--- services/account/src/main/java/com/amerbank/account/repository/AccountRepository.java
+++ services/account/src/main/java/com/amerbank/account/repository/AccountRepository.java
@@ -1,7 +1,10 @@
 package com.amerbank.account.repository;
 
 import com.amerbank.account.model.Account;
+import com.amerbank.account.model.AccountType;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 
 import java.util.List;
 import java.util.Optional;
@@ -10,7 +13,10 @@ import java.util.UUID;
 public interface AccountRepository extends JpaRepository<Account, UUID> {
     boolean existsByAccountNumber(String accountNumber);
     Optional<Account> findByAccountNumber(String accountNumber);
-    Optional<Account> findByCustomerId(Long CustomerID);
 
     List<Account> findAllByCustomerId(Long customerId);
+
+    boolean existsByCustomerIdAndType(Long customerId, @NotNull AccountType type);
+
+    Optional<Account> findByCustomerIdAndType(Long customerId, AccountType type);
 }
