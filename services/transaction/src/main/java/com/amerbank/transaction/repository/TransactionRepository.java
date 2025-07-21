6ee1022e6d58644062626cf3e1b0a20ebf746cfc
lfav07 <lfavieira07@gmail.com>
2025-07-21 18:24:55 -0300

    feat(transaction) defined transaction repository

---
+++ services/transaction/src/main/java/com/amerbank/transaction/repository/TransactionRepository.java
@@ -0,0 +1,44 @@
+package com.amerbank.transaction.repository;
+
+import com.amerbank.transaction.model.Transaction;
+import com.amerbank.transaction.model.TransactionStatus;
+import com.amerbank.transaction.model.TransactionType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
+
+    List<Transaction> findByFromAccountNumberOrToAccountNumber(String fromAccount, String toAccount);
+
+
+    List<Transaction> findByFromAccountNumber(String fromAccount);
+
+
+    List<Transaction> findByToAccountNumber(String toAccount);
+
+
+    List<Transaction> findByStatus(TransactionStatus status);
+
+
+    List<Transaction> findByFromAccountNumberAndStatusOrderByCreatedAtDesc(String fromAccount, TransactionStatus status);
+
+
+    List<Transaction> findByCreatedAtBetween(LocalDateTime start, LocalDateTime end);
+
+
+    long countByType(TransactionType type);
+
+
+    List<Transaction> findByFromAccountNumberAndStatus(String fromAccount, TransactionStatus status);
+
+
+    Optional<Transaction> findByIdAndFromAccountNumber(Long id, String fromAccount);
+
+
+    List<Transaction> findTop5ByFromAccountNumberOrderByCreatedAtDesc(String fromAccount);
+}
