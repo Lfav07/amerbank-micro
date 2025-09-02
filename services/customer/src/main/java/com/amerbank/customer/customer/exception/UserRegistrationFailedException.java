3563a9753e0a08e434c6b6b2ee4d423341a07758
lfav07 <lfavieira07@gmail.com>
2025-09-02 19:08:06 -0300

    feat(customer) added custom exception

---
+++ services/customer/src/main/java/com/amerbank/customer/customer/exception/UserRegistrationFailedException.java
@@ -0,0 +1,7 @@
+package com.amerbank.customer.customer.exception;
+
+public class UserRegistrationFailedException extends RuntimeException {
+    public UserRegistrationFailedException(String s) {
+        super(s);
+    }
+}
