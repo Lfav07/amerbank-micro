a2babcf6f9ef2bcd91b493774158d5678e445c98
lfav07 <lfavieira07@gmail.com>
2025-09-30 18:05:10 -0300

    fix(auth) annotated mapper with @Service for spring to instantiate class

--- services/auth-server/src/main/java/com/amerbank/auth_server/service/UserMapper.java
+++ services/auth-server/src/main/java/com/amerbank/auth_server/service/UserMapper.java
@@ -2,7 +2,10 @@ package com.amerbank.auth_server.service;
 
 import com.amerbank.auth_server.model.User;
 import com.amerbank.common_dto.UserResponse;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserMapper {
 
     public UserResponse toResponse(User user) {
