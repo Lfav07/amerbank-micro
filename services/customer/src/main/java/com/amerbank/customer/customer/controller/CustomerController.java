4816ec4dde709f96a514153e9156ff66488b1868
lfav07 <lfavieira07@gmail.com>
2026-01-15 01:59:27 -0300

    refactor(customer): Controller refactor to align with refactored service
    Transfer admin logic to separate controller

--- services/customer/src/main/java/com/amerbank/customer/customer/controller/CustomerController.java
+++ services/customer/src/main/java/com/amerbank/customer/customer/controller/CustomerController.java
@@ -37,84 +37,10 @@ public class CustomerController {
     }
 
 
-    @GetMapping("/{id}")
-    public ResponseEntity<CustomerResponse> getCustomerById(@PathVariable Long id) {
-        CustomerResponse response = service.getCustomerInfo(id);
-        return ResponseEntity.ok(response);
-    }
 
     @GetMapping("/me")
     public ResponseEntity<CustomerInfo> getMyProfile(@AuthenticationPrincipal JwtUserPrincipal principal) {
-
-        CustomerResponse resp = service.getMyCustomerInfoById(principal.customerId());
-        CustomerInfo info = mapper.getInfoFromCustomer(resp);
-
-        return ResponseEntity.ok(info);
-    }
-
-    @GetMapping("/get/{email}")
-    public ResponseEntity<CustomerResponse> getCustomerByEmail(
-            HttpServletRequest request,
-            @PathVariable @Email String email) {
-
-        String authHeader = request.getHeader("Authorization");
-        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-        }
-
-        String jwtToken = authHeader.substring(7);
-        CustomerResponse resp = service.getCustomerInfoByEmail(email, jwtToken);
+        CustomerInfo resp = service.getMyCustomerInfo(principal.customerId());
         return ResponseEntity.ok(resp);
-    }
-
-
-    @GetMapping("/user/{userId}")
-    public ResponseEntity<CustomerResponse> getCustomerByUserId(@PathVariable Long userId) {
-        CustomerResponse response = service.getCustomerInfoByUserId(userId);
-        return ResponseEntity.ok(response);
-    }
-
-    @GetMapping("/get/id/user/{userId}")
-    public ResponseEntity<Long> getCustomerIdByUserId(@PathVariable Long userId) {
-        Long id = service.getCustomerIdByUserId(userId);
-        return ResponseEntity.ok(id);
-    }
-
-    @GetMapping("/get/all")
-    public ResponseEntity<List<Customer>> getAllCustomersInfo() {
-        List<Customer> responseList = service.findAllCustomers();
-        return ResponseEntity.ok(responseList);
-    }
-
-    @PutMapping("/update")
-    public ResponseEntity<CustomerResponse> updateCustomer(@RequestBody CustomerUpdateRequest request) {
-        CustomerResponse updated = service.editCustomerInfo(request);
-        return ResponseEntity.ok(updated);
-    }
-
-
-    @PatchMapping("/{id}/verify-kyc")
-    public ResponseEntity<String> verifyKyc(@PathVariable Long id) {
-        service.verifyKyc(id);
-        return ResponseEntity.ok("KYC verified successfully");
-    }
-
-    @PatchMapping("/{id}/unverify-kyc")
-    public ResponseEntity<String> unVerifyKyc(@PathVariable Long id) {
-        service.unVerifyKyc(id);
-        return ResponseEntity.ok("KYC unverified successfully");
-    }
-
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<String> deleteCustomer(@PathVariable Long id) {
-        service.deleteCustomerById(id);
-        return ResponseEntity.ok("Customer deleted successfully");
-    }
-
-    @DeleteMapping("/all/delete")
-    public ResponseEntity<String> deleteAllCustomers() {
-        service.deleteAllCustomers();
-        return ResponseEntity.ok("Customers deleted successfully");
     }
 }
