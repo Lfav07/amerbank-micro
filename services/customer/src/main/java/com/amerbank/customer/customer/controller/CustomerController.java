249f5475a8f7e15c080030e64ac23b1ce5efbd2d
lfav07 <lfavieira07@gmail.com>
2026-01-17 23:30:35 -0300

    refactor(customer): remove unused imports

--- services/customer/src/main/java/com/amerbank/customer/customer/controller/CustomerController.java
+++ services/customer/src/main/java/com/amerbank/customer/customer/controller/CustomerController.java
@@ -2,25 +2,14 @@ package com.amerbank.customer.customer.controller;
 
 import com.amerbank.customer.customer.dto.CustomerInfo;
 import com.amerbank.customer.customer.dto.CustomerRequest;
-import com.amerbank.customer.customer.dto.CustomerResponse;
-import com.amerbank.customer.customer.dto.CustomerUpdateRequest;
-import com.amerbank.customer.customer.model.Customer;
 import com.amerbank.customer.customer.security.JwtUserPrincipal;
-import com.amerbank.customer.customer.service.CustomerMapper;
 import com.amerbank.customer.customer.service.CustomerService;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.Email;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-
+import java.util.Map;
 
 @RequiredArgsConstructor
 @RestController
@@ -28,12 +17,11 @@ import java.util.List;
 public class CustomerController {
 
     private final CustomerService service;
-    private final CustomerMapper mapper;
 
     @PostMapping("/register")
-    public ResponseEntity<?> registerCustomer(@RequestBody CustomerRequest request) {
+    public ResponseEntity<Map<String, String>> registerCustomer(@RequestBody @Valid CustomerRequest request) {
         service.registerCustomer(request);
-        return ResponseEntity.ok("Customer successfully registered");
+        return ResponseEntity.ok(Map.of("message", "Customer successfully registered"));
     }
 
 
