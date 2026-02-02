0d4a92b72be99dbd3903ca07bd6e08bf532690e8
lfav07 <lfavieira07@gmail.com>
2026-02-01 21:05:43 -0300

    refactor(transaction) implement new endpoints that are accessible to the customer, transfer admin-specific methods to TransactionAdminController

--- services/transaction/src/main/java/com/amerbank/transaction/controller/TransactionController.java
+++ services/transaction/src/main/java/com/amerbank/transaction/controller/TransactionController.java
@@ -1,118 +1,85 @@
 package com.amerbank.transaction.controller;
 
 import com.amerbank.transaction.dto.*;
-import com.amerbank.transaction.model.Transaction;
 import com.amerbank.transaction.model.TransactionStatus;
 import com.amerbank.transaction.model.TransactionType;
 import com.amerbank.transaction.security.JwtUserPrincipal;
-import com.amerbank.transaction.service.TransactionMapper;
 import com.amerbank.transaction.service.TransactionService;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.UUID;
 
 @RestController
-@RequestMapping("/transactions")
+@RequestMapping("/transaction")
 @AllArgsConstructor
 public class TransactionController {
 
     private final TransactionService transactionService;
-    private final TransactionMapper transactionMapper;
 
-    String extractJwt(String authorization){
-        return authorization.replace("Bearer ", "");
-    }
-
-    // --- GET TRANSACTIONS ---
-
-    @GetMapping("/{id}")
-    public ResponseEntity<TransactionResponse> getTransactionById(@PathVariable UUID id) {
-        Transaction transaction = transactionService.findTransactionById(id);
-        return ResponseEntity.ok(transactionMapper.toResponse(transaction));
-    }
-
-    @GetMapping("/from-account/{fromAccount}")
-    public ResponseEntity<List<TransactionResponse>> getByFromAccount(@PathVariable String fromAccount) {
-        return ResponseEntity.ok(
-                transactionService.findTransactionsByFromAccountNumber(fromAccount)
-                        .stream().map(transactionMapper::toResponse).toList()
-        );
-    }
-
-    @GetMapping(params = {"fromAccount", "toAccount"})
-    public ResponseEntity<List<TransactionResponse>> getByFromAndToAccount(
-            @RequestParam String fromAccount,
-            @RequestParam String toAccount) {
-        List<TransactionResponse> response = transactionService
-                .findTransactionsByFromAndToAccountNumber(fromAccount, toAccount)
-                .stream()
-                .map(transactionMapper::toResponse)
-                .toList();
-
-        return ResponseEntity.ok(response);
-    }
-
-
-    @GetMapping("/from/{fromAccount}/to/{toAccount}")
-    public ResponseEntity<List<TransactionResponse>> getByFromAndTo(
-            @PathVariable String fromAccount,
-            @PathVariable String toAccount
-    ) {
-        List<Transaction> list = transactionService.findTransactionsByFromAndToAccountNumber(fromAccount, toAccount);
-        return ResponseEntity.ok(
-                list.stream().map(transactionMapper::toResponse).toList()
-        );
-    }
-
-
-    @GetMapping("/to-account/{toAccount}")
-    public ResponseEntity<List<TransactionResponse>> getByToAccount(@PathVariable String toAccount) {
-        return ResponseEntity.ok(
-                transactionService.findTransactionsByToAccountNumber(toAccount)
-                        .stream().map(transactionMapper::toResponse).toList()
-        );
-    }
-
-
-    @GetMapping("/status/{status}")
-    public ResponseEntity<List<TransactionResponse>> getByStatus(@PathVariable TransactionStatus status) {
-        return ResponseEntity.ok(
-                transactionService.findTransactionsByStatus(status)
-                        .stream().map(transactionMapper::toResponse).toList()
-        );
-    }
-
-
-    @GetMapping("/type/{type}")
-    public ResponseEntity<List<TransactionResponse>> getByType(@PathVariable TransactionType type) {
-        return ResponseEntity.ok(
-                transactionService.findTransactionsByType(type)
-                        .stream().map(transactionMapper::toResponse).toList()
-        );
-    }
-
-
-    @GetMapping("/account/{accountNumber}/me")
+    @GetMapping("/me")
     public ResponseEntity<List<TransactionResponse>> getMyTransactions(
             @AuthenticationPrincipal JwtUserPrincipal jwtUserPrincipal,
-            @PathVariable String accountNumber
+            @RequestParam @NotBlank String accountNumber
     ) {
-       Long customerId = jwtUserPrincipal.customerId();
-        return ResponseEntity.ok(
-                transactionService.getMyTransactions(customerId, accountNumber)
-                        .stream().map(transactionMapper::toResponse).toList()
-        );
+        Long customerId = jwtUserPrincipal.customerId();
+        return ResponseEntity.ok(transactionService.getMyTransactionResponses(customerId, accountNumber));
     }
-    // --- CREATE TRANSACTIONS ---
 
-    @PostMapping("/user/deposit")
+    @GetMapping("/me/from")
+    public ResponseEntity<List<TransactionResponse>> getMyTransactionsByFromAccount(
+            @AuthenticationPrincipal JwtUserPrincipal jwtUserPrincipal,
+            @RequestParam @NotBlank String fromAccountNumber
+    ) {
+        Long customerId = jwtUserPrincipal.customerId();
+        return ResponseEntity.ok(transactionService.getMyTransactionsByFromAccountResponses(customerId, fromAccountNumber));
+    }
+
+    @GetMapping("/me/to")
+    public ResponseEntity<List<TransactionResponse>> getMyTransactionsByToAccount(
+            @AuthenticationPrincipal JwtUserPrincipal jwtUserPrincipal,
+            @RequestParam @NotBlank String toAccountNumber
+    ) {
+        Long customerId = jwtUserPrincipal.customerId();
+        return ResponseEntity.ok(transactionService.getMyTransactionsByToAccountResponses(customerId, toAccountNumber));
+    }
+
+    @GetMapping("/me/transfer")
+    public ResponseEntity<List<TransactionResponse>> getMyTransactionsByFromAndToAccount(
+            @AuthenticationPrincipal JwtUserPrincipal jwtUserPrincipal,
+            @RequestParam @NotBlank String fromAccountNumber,
+            @RequestParam @NotBlank String toAccountNumber
+    ) {
+        Long customerId = jwtUserPrincipal.customerId();
+        return ResponseEntity.ok(transactionService.getMyTransactionsByFromAndToAccountResponses(customerId, fromAccountNumber, toAccountNumber));
+    }
+
+    @GetMapping("/me/status")
+    public ResponseEntity<List<TransactionResponse>> getMyTransactionsByStatus(
+            @AuthenticationPrincipal JwtUserPrincipal jwtUserPrincipal,
+            @RequestParam @NotBlank String accountNumber,
+            @RequestParam @NotNull TransactionStatus status
+    ) {
+        Long customerId = jwtUserPrincipal.customerId();
+        return ResponseEntity.ok(transactionService.getMyTransactionsByStatusResponses(customerId, accountNumber, status));
+    }
+
+    @GetMapping("/me/type")
+    public ResponseEntity<List<TransactionResponse>> getMyTransactionsByType(
+            @AuthenticationPrincipal JwtUserPrincipal jwtUserPrincipal,
+            @RequestParam @NotBlank String accountNumber,
+            @RequestParam @NotNull TransactionType type
+    ) {
+        Long customerId = jwtUserPrincipal.customerId();
+        return ResponseEntity.ok(transactionService.getMyTransactionsByTypeResponses(customerId, accountNumber, type));
+    }
+
+    @PostMapping("/deposit")
     public ResponseEntity<TransactionResponse> createDeposit(
             @AuthenticationPrincipal JwtUserPrincipal jwtUserPrincipal,
             @RequestHeader("idempotency-key") @NotBlank String idempotencyKey,
@@ -123,7 +90,7 @@ public class TransactionController {
         return ResponseEntity.ok(response);
     }
 
-    @PostMapping("/user/payment")
+    @PostMapping("/payment")
     public ResponseEntity<TransactionResponse> createPayment(
             @AuthenticationPrincipal JwtUserPrincipal jwtUserPrincipal,
             @RequestHeader("idempotency-key") @NotBlank String idempotencyKey,
@@ -134,11 +101,11 @@ public class TransactionController {
         return ResponseEntity.ok(response);
     }
 
-    @PostMapping("/user/refund")
+    @PostMapping("/refund")
     public ResponseEntity<TransactionResponse> createRefund(
             @AuthenticationPrincipal JwtUserPrincipal jwtUserPrincipal,
             @RequestHeader("idempotency-key") @NotBlank String idempotencyKey,
-           @Valid @RequestBody RefundTransactionRequest request
+            @Valid @RequestBody RefundTransactionRequest request
     ) {
         Long customerId = jwtUserPrincipal.customerId();
         TransactionResponse response = transactionService.createRefundTransaction(customerId, idempotencyKey, request);
