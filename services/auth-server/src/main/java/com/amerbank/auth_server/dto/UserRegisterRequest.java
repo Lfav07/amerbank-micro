3742bcd2aec61dea428cbbdffddcc0af7f9d83d0
lfav07 <lfavieira07@gmail.com>
2026-02-11 23:36:16 -0300

    refactor(auth) user registration dto now contains costumer data

--- services/auth-server/src/main/java/com/amerbank/auth_server/dto/UserRegisterRequest.java
+++ services/auth-server/src/main/java/com/amerbank/auth_server/dto/UserRegisterRequest.java
@@ -3,9 +3,22 @@ package com.amerbank.auth_server.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+
+import java.time.LocalDate;
 
 public record UserRegisterRequest(
         @Email String email,
-        @NotBlank  @Size(min = 4, message = "Password too short")  String password
-) {}
+        @NotBlank @Size(min = 4, message = "Password too short") String password,
+
+        @NotBlank(message = "First name is required")
+        String firstName,
+-
+        @NotBlank(message = "Last name is required")
+        String lastName,
+
+        @NotNull(message = "Date of birth is required")
+        LocalDate dateOfBirth
+) {
+}
