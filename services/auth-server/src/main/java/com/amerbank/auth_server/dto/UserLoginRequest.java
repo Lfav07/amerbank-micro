43becc19d0fe23d9c043622f4f3d3279efa9f9c6
lfav07 <lfavieira07@gmail.com>
2026-02-11 22:40:25 -0300

    refactor(auth) improve dto validation

--- services/auth-server/src/main/java/com/amerbank/auth_server/dto/UserLoginRequest.java
+++ services/auth-server/src/main/java/com/amerbank/auth_server/dto/UserLoginRequest.java
@@ -1,6 +1,9 @@
 package com.amerbank.auth_server.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+
 public record UserLoginRequest(
-        String email,
-        String password
+     @NotBlank String email,
+     @NotBlank String password
 ) {}
