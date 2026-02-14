18b78036998724ff44ddfbea88fd20a7ccfaab10
lfav07 <lfavieira07@gmail.com>
2026-02-14 15:27:22 -0300

    refactor(customer) allow multiple configuration properties beans to exist

--- services/customer/src/main/java/com/amerbank/customer/CustomerApplication.java
+++ services/customer/src/main/java/com/amerbank/customer/CustomerApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(CustomerProperties.class)
 public class CustomerApplication {
 
 	public static void main(String[] args) {
--- services/customer/src/main/java/com/amerbank/customer/customer/config/CustomerProperties.java
+++ services/customer/src/main/java/com/amerbank/customer/customer/config/CustomerProperties.java
@@ -5,9 +5,11 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
 @Getter
 @Setter
+@Component
 @ConfigurationProperties(prefix = "customer")
 public class CustomerProperties {
 
