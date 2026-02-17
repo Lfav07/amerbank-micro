1848ab9405b6e0aa74e54c34a54715e22cc45688
lfav07 <lfavieira07@gmail.com>
2026-02-17 18:02:55 -0300

    fix(account) fix redis serialization issues

--- services/account/src/main/java/com/amerbank/account/config/RedisConfig.java
+++ services/account/src/main/java/com/amerbank/account/config/RedisConfig.java
@@ -1,24 +1,68 @@
 package com.amerbank.account.config;
 
+import com.amerbank.account.dto.AccountResponse;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.type.CollectionType;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericToStringSerializer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Map;
 
 @Configuration
 @EnableCaching
 public class RedisConfig {
 
     @Bean
-    public RedisCacheConfiguration cacheConfiguration(ObjectMapper objectMapper) {
-        return RedisCacheConfiguration.defaultCacheConfig()
+    public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+
+        Jackson2JsonRedisSerializer<AccountResponse> accountSerializer =
+                new Jackson2JsonRedisSerializer<>(mapper, AccountResponse.class);
+
+        RedisCacheConfiguration accountByNumberConfig = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(60))
+                .disableCachingNullValues()
+                .serializeKeysWith(
+                        RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer())
+                )
                 .serializeValuesWith(
-                        RedisSerializationContext.SerializationPair.fromSerializer(
-                                new GenericJackson2JsonRedisSerializer(objectMapper)
-                        )
+                        RedisSerializationContext.SerializationPair.fromSerializer(accountSerializer)
                 );
+        CollectionType accountListType = mapper.getTypeFactory()
+                .constructCollectionType(List.class, AccountResponse.class);
+
+        Jackson2JsonRedisSerializer<List<AccountResponse>> accountListSerializer =
+                new Jackson2JsonRedisSerializer<>(mapper, accountListType);
+
+        RedisCacheConfiguration accountsByCustomerConfig = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(60))
+                .disableCachingNullValues()
+                .serializeKeysWith(
+                        RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer())
+                )
+                .serializeValuesWith(
+                        RedisSerializationContext.SerializationPair.fromSerializer(accountListSerializer)
+                );
+
+        return RedisCacheManager.builder(connectionFactory)
+                .withCacheConfiguration("account-by-number", accountByNumberConfig)
+                .withCacheConfiguration("accounts-by-customer", accountsByCustomerConfig)
+                .build();
     }
 }
