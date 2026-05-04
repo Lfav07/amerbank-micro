3b224d08e8e6887baa5281d20ab5b15f1dfc9e3d
lfav07 <lfavieira07@gmail.com>
2026-05-04 18:57:15 -0300

    refactor: move json converter from utils to model package

--- services/audit-logging/src/main/java/com/amerbank/audit_logging/model/JsonConverter.java
+++ services/audit-logging/src/main/java/com/amerbank/audit_logging/model/JsonConverter.java
@@ -1,4 +1,4 @@
-package com.amerbank.audit_logging.util;
+package com.amerbank.audit_logging.model;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
