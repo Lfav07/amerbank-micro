a0a56716bec3dd64d7ac7459ca7092cfb9f5f37a
lfav07 <lfavieira07@gmail.com>
2026-05-05 01:36:36 -0300

    chore: update kafka consumer to access 29092 kafka port, following containerization best practices

--- services/audit-logging/src/main/java/com/amerbank/audit_logging/config/KafkaConsumerConfig.java
+++ services/audit-logging/src/main/java/com/amerbank/audit_logging/config/KafkaConsumerConfig.java
@@ -74,7 +74,7 @@ public class KafkaConsumerConfig {
     @Bean
     public Map<String, Object> consumerConfigs() {
         Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:29092");
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "audit-service");
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JacksonJsonDeserializer.class);
