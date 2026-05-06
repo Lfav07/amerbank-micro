889ccf3c2e0feae39b451f8fad962c8f74d98a45
lfav07 <lfavieira07@gmail.com>
2026-05-06 17:57:57 -0300

    refactor: allow actuator endpoint public access in security config

--- services/audit-logging/src/main/java/com/amerbank/audit_logging/security/SecurityConfig.java
+++ services/audit-logging/src/main/java/com/amerbank/audit_logging/security/SecurityConfig.java
@@ -25,6 +25,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
 
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/actuator/health").permitAll()
                         .anyRequest().hasRole("ADMIN")
                 )
 
